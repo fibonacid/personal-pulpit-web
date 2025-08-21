c0f4b3b3c27835c1966937b70819b1f608466947
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-08-22 01:32:20 +0200

    add register form

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,5 +1,8 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { signIn, useSignIn } from '@/lib/api/fetchers/signin'
 import { createFileRoute } from '@tanstack/react-router'
+import { useCallback, useState, type FormEventHandler } from 'react'
 
 export const Route = createFileRoute('/register')({
 	component: Register,
@@ -7,27 +10,71 @@ export const Route = createFileRoute('/register')({
 
 function Register() {
 	const { mutate } = useSignIn()
-	//  biography?: string;
-	// email: string;
-	// firstName: string;
-	// lastName?: string;
-	// password: string;
+
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+
+	const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>((event) => {
+		event.preventDefault();
+		if (password !== confirmPassword) {
+			alert('Passwords do not match');
+			return;
+		}
+		mutate({
+			firstName,
+			lastName,
+			email,
+			password,
+		}, {
+			onSuccess: (data) => {
+				alert('Registration successful!');
+				console.log(data);
+			},
+			onError: (error) => {
+				alert('Registration failed: ' + error.message);
+				console.error(error);
+			}
+		});
+	}, [password, confirmPassword]);
+
 	return (
 		<div className="m-4" >
 			<h1>Register</h1>
-			<form>
+			<form className="flex flex-col gap-2" onSubmit={handleSubmit}>
 				{/* -- first name -- */}
-				<div />
+				<Input placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}
+					autoComplete="given-name"
+					required
+				/>
 				{ /* -- last name -- */}
-				<div />
+				<Input placeholder="Last Name"
+					onChange={(e) => setLastName(e.target.value)}
+					autoComplete="family-name"
+					required
+				/>
 				{/* -- email -- */}
-				<div />
+				<Input placeholder="Email" type="email"
+					onChange={(e) => setEmail(e.target.value)}
+					autoComplete="email"
+					required
+				/>
 				{ /* -- password -- */}
-				<div />
+				<Input placeholder="Password" type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					autoComplete="new-password"
+					required
+				/>
 				{/* -- confirm password -- */}
-				<div />
+				<Input placeholder="Confirm Password" type="password"
+					onChange={(e) => setConfirmPassword(e.target.value)}
+					autoComplete="new-password"
+					required
+				/>
 				{/* -- submit button -- */}
-				<button>Submit</button>
+				<Button>Submit</Button>
 			</form>
 		</div >
 	)
